310630c2078b388495a8a75be5ecb28ed1ca4c8b
ycachy <1021487797@qq.com>
2018-11-19 17:22:52 +0200

    Add files via upload

---
+++ startup.m
@@ -0,0 +1,7 @@
+run('C:\Users\ChenKx\Desktop\FFWTensor-master\pkg\vlfeat-0.9.20\toolbox\vl_setup.m');
+addpath('C:\Users\ChenKx\Desktop\FFWTensor-master\pkg\')
+addpath('C:\Users\ChenKx\Desktop\FFWTensor-master\pkg\tensor_toolbox_2.6');
+addpath('C:\Users\ChenKx\Desktop\FFWTensor-master\pkg\tensor_toolbox_2.6\met');
+addpath('C:\Users\ChenKx\Desktop\FFWTensor-master\pkg\PROPACK')
+addpath(genpath('src'));
+addpath(genpath('demo'));
