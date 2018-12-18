237aaf4eaafe82e0e4fd6219c5428cea0acfe553
ycachy <1021487797@qq.com>
2018-12-18 22:45:16 +0200

    Update Local_Update.m

--- DP_FW/Local_Update.m
+++ DP_FW/Local_Update.m
@@ -1,7 +1,6 @@
 function [Yi,AN]=Local_Update(i,v,lamda1,T,t,L,D,k,Omega,Yi)
 
 Y1=D(i,:);
-Omegai=Omega(i,:);
 [~,n]=size(D);
 
 if t==1
@@ -12,8 +11,8 @@ end
 A=omega(Y-Y1,Omegai);
 u=(A.*v)/lamda1;
 a = (1-1/T)*Y-k/T*u*v';
-Y=projection(a,L,Omegai);
-A=omega(Y-Y1,Omegai);
+Y=projection(a,L);
+A=Y-Y1;
 AN=A.*A';
 Yi=Y;
 
