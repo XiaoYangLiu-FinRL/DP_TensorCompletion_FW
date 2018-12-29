98b5deed9120abb49f015685c634c02cd14be48c
ycachy <1021487797@qq.com>
2018-12-29 17:49:33 +0200

    Add files via upload

--- matrix_completion/FW.py
+++ matrix_completion/FW.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import random
 def Synthetic():
    # u=np.zeros((500000,1),np.dtype='float16')
-    u = np.random.random((50000, 1))
+    u = np.random.random((5000, 1))
     v = np.random.random((40, 1))
     YYY = np.dot(u, np.transpose(v))
     return YYY
@@ -28,8 +28,7 @@ def rmse(target,prediction):
         squaredError.append(val * val)
         absError.append(abs(val))
     #print len(squaredError)
-    (m,n)=np.shape(Xx)
-    X=np.zeros((m,n))
+
     rm=np.sqrt(sum(squaredError) / len(squaredError))
    # print sum(squaredError)
     return np.sqrt(sum(squaredError) / len(squaredError))
@@ -45,15 +44,32 @@ def Omegamatrix(Xx,samplenum):
             X[i][j]=1
     Result=X*Xx
     return Result
-T=20
+
+
+def processU(mmatrixU,saveU):
+    documentU=[]
+    for line in mmatrixU:
+        linenew=line[:saveU]
+        documentU.append(linenew)
+    return documentU
+
+def processV(mmatrixV,saveV):
+    documentV=mmatrixV[:saveV]
+    return documentV
+
+
+
+
+
+
 
 datainput1=Synthetic()
 #datainput2=dataprocess(datainput1)
 #L = maxnorm(datainput)
 #L=math.pow(n,1.0/4)
-T = 5
+T = 4
 #belta = 1
-datainput=Omegamatrix(datainput1,80)
+datainput=Omegamatrix(datainput1,7)
 #L = maxnorm(datainput)
 k = np.linalg.matrix_rank(datainput1)
 #print L
@@ -63,13 +79,13 @@ k = np.linalg.matrix_rank(datainput1)
 Yt=np.zeros((m,n))
 for i in range(T):
     wt = Yt - datainput
-    u,s,v = np.linalg.svd(wt)
-    uu = u[:,1]
-  #  ss = s[1,:]
-    vv = v[:,1]
-    print v.shape
-    print u.shape
-    st = np.dot(uu, np.transpose(vv))
+    U,S,V = np.linalg.svd(wt)
+
+  #
+    uu=processU(U,1)
+    vv=processV(V,1)
+    #print vv
+    st = np.dot(uu,vv)
   #  st=np.dot(uu,vv)
 
     Yt = (1-1.0/T)*Yt-float(k)/T*st
